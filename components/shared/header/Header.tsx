c0293a7362d9522d4f9e7df629a089140b61d240
Yolosopher <nika.nishnianidze.10@gmail.com>
2024-05-23 00:15:23 +0400

    Styled: made header logo smaller on sm viewport

--- components/shared/header/Header.tsx
+++ components/shared/header/Header.tsx
@@ -8,18 +8,15 @@ const Header = ({ settings }: { settings: ISetting }) => {
   return (
     <header className="h-24 flex items-center fixed z-10 top-0 left-0 w-full bg-background shadow-primary/10 shadow-xl">
       <div className="container flex items-center justify-between">
-        <Link href="/">
+        <Link href="/" className="flex w-[200px] sm:w-[246px] aspect-[246/60]">
           <Image
             src="/logo.png"
             priority
             alt="Logo"
-            className="h-12 xl:h-[60px] object-contain w-max"
+            className="object-contain w-max h-max"
             width={246}
             height={60}
-            style={{
-              width: "auto",
-              height: "auto",
-            }}
+            style={{ width: "auto", height: "auto" }}
           />
         </Link>
         <div className="flex gap-2 xl:gap-[3.125rem]  items-center text-xl font-medium">
