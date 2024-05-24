18d2af60b15b651d91223d4d4234e893bcf93c68
Yolosopher <nika.nishnianidze.10@gmail.com>
2024-05-24 14:08:35 +0400

    Added: hover infos for Tech Items

--- components/tech/TechItem.tsx
+++ components/tech/TechItem.tsx
@@ -1,6 +1,14 @@
 import { ITechStack } from "@/models/tech";
 import RenderImage from "../shared/image/RenderImage";
 import { cn } from "@/lib/utils";
+import { HoverCard, HoverCardTrigger } from "../ui/hover-card";
+import { HoverCardContent } from "@radix-ui/react-hover-card";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "../ui/tooltip";
 
 interface TechItemProps extends ITechStack {
   hidden?: boolean;
@@ -16,16 +24,25 @@ const TechItem = ({
   const invertOnDark = description && description.includes("invert");
   return hidden ? null : (
     <li className="max-w-[7.5rem] w-full aspect-square flex justify-center items-center">
-      <RenderImage
-        name={icon}
-        alt={name}
-        width={120}
-        height={120}
-        className={cn(
-          "max-w-[7.5rem] w-full",
-          invertOnDark ? "dark:filter dark:invert" : ""
-        )}
-      />
+      <TooltipProvider delayDuration={300}>
+        <Tooltip>
+          <TooltipTrigger>
+            <RenderImage
+              name={icon}
+              alt={name}
+              width={120}
+              height={120}
+              className={cn(
+                "max-w-[7.5rem] w-full",
+                invertOnDark ? "dark:filter dark:invert" : ""
+              )}
+            />
+          </TooltipTrigger>
+          <TooltipContent>
+            <p className="text-base select-auto capitalize">{name}</p>
+          </TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
     </li>
   );
 };
