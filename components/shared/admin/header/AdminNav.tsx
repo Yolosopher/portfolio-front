c362e4a3362a432cc282fbd9558b33f0396009bb
Yolosopher <nika.nishnianidze.10@gmail.com>
2024-06-26 06:39:00 +0400

    chore: Add CheckToken component to AdminNav for authentication handling

--- components/shared/admin/header/AdminNav.tsx
+++ components/shared/admin/header/AdminNav.tsx
@@ -1,5 +1,7 @@
+"use client";
 import navListAdmin from "@/config/adminNav";
 import Link from "next/link";
+import CheckToken from "./CheckToken";
 
 const NavLi = ({ name, path }: { name: string; path: string }) => {
   return (
@@ -11,13 +13,16 @@ const NavLi = ({ name, path }: { name: string; path: string }) => {
 
 const AdminNav = () => {
   return (
-    <nav className="flex w-1 md:w-full flex-1">
-      <ul className="flex gap-4 md:justify-center whitespace-nowrap capitalize w-full overflow-x-auto py-1">
-        {navListAdmin.map((nav, index) => (
-          <NavLi {...nav} key={index} />
-        ))}
-      </ul>
-    </nav>
+    <>
+      <CheckToken />
+      <nav className="flex w-1 md:w-full flex-1">
+        <ul className="flex gap-4 md:justify-center whitespace-nowrap capitalize w-full overflow-x-auto py-1">
+          {navListAdmin.map((nav, index) => (
+            <NavLi {...nav} key={index} />
+          ))}
+        </ul>
+      </nav>
+    </>
   );
 };
 export default AdminNav;
---
+++ components/shared/admin/header/CheckToken.tsx
@@ -0,0 +1,48 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+"use client";
+
+import { toast } from "@/components/ui/use-toast";
+import useErrorHandler from "@/hooks/error-handler/useErrorHandler";
+import useApiRequest from "@/hooks/request/useApiRequest";
+import { useContext, useEffect } from "react";
+import { authContext } from "../../../../context/authContext";
+
+const CheckToken = () => {
+  const authCtx = useContext(authContext);
+
+  const token = authCtx?.token;
+  const request = useApiRequest();
+  const errorHandler = useErrorHandler();
+  useEffect(() => {
+    const checkTokenRunner = async () => {
+      try {
+        console.log("Checking token");
+        const result = await request({
+          url: "/auth/self",
+          auth: true,
+          method: "GET",
+        });
+
+        console.log(result);
+
+        if (result) {
+          if (!result.success) {
+            errorHandler(result.error);
+          } else {
+            toast({
+              title: "Success",
+              description: result.data.message ?? "Token is valid",
+            });
+          }
+        }
+      } catch (error: any) {
+        errorHandler(error.message);
+      }
+    };
+
+    checkTokenRunner();
+  }, [token]);
+  return null;
+};
+
+export default CheckToken;
