2583786adf1958e72abbd3ab4e3648974a80bc7e
Nika <nika.nishnianidze.10@gmail.com>
2024-08-11 07:01:26 +0400

    Update text alignment for name in ShowLevelAndName component

--- components/tech/ShowLevelAndName.tsx
+++ components/tech/ShowLevelAndName.tsx
@@ -24,7 +24,7 @@ const ShowLevelAndName = ({ name, level, max }: ShowLevelAndNameProps) => {
                 indicatorClassName="bg-primary transition-translate-x duration-300 ease-[cubic-bezier(0.1, -0.6, 0.2, 0)]"
             />
             <div className="w-full flex items-center justify-between gap-1">
-                <h3 className="text-sm font-bold whitespace-nowrap">{name}</h3>
+                <h3 className="text-sm font-bold text-left">{name}</h3>
                 <p className="font-bold w-full text-end">
                     {level}/{max}
                 </p>
