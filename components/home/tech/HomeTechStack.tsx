ee083b91e6268554f5c221722b11774f8dfd2214
Yolosopher <nika.nishnianidze.10@gmail.com>
2024-08-24 14:37:39 +0400

    Fixed animation for Progressbars in TechItem component

--- components/home/tech/HomeTechStack.tsx
+++ components/home/tech/HomeTechStack.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 "use client";
 import TechContent, { TechContentProps } from "@/components/tech/TechContent";
 import SectionTitle from "../section-title/SectionTitle";
@@ -10,68 +11,72 @@ import { Button } from "@/components/ui/button";
 import Link from "next/link";
 
 const HomeTechStack = ({
-  techs,
-  separate,
+    techs,
+    separate,
 }: Omit<TechContentProps, "active" | "sizeInfo"> & {
-  separate?: boolean;
+    separate?: boolean;
 }) => {
-  const { updateViewInfo } = useContext(inViewContext);
-  const mediaSize = useMediaSize();
+    const { updateViewInfo } = useContext(inViewContext);
+    const mediaSize = useMediaSize();
 
-  const sizeInfo = useMemo(() => {
-    if (mediaSize === "xl")
-      return {
-        hiddenNumber: 11,
-        cols: 5,
-      };
-    if (mediaSize === "lg")
-      return {
-        hiddenNumber: 9,
-        cols: 4,
-      };
-    if (mediaSize === "md")
-      return {
-        hiddenNumber: 7,
-        cols: 3,
-      };
-    return {
-      hiddenNumber: 7,
-      cols: 2,
-    };
-  }, [mediaSize]);
-  const { ref, inView } = useInView({
-    /* Optional options */
-    threshold: 0,
-    // triggerOnce: true,
-  });
+    const sizeInfo = useMemo(() => {
+        if (mediaSize === "xl")
+            return {
+                hiddenNumber: 11,
+                cols: 5,
+            };
+        if (mediaSize === "lg")
+            return {
+                hiddenNumber: 9,
+                cols: 4,
+            };
+        if (mediaSize === "md")
+            return {
+                hiddenNumber: 7,
+                cols: 3,
+            };
+        return {
+            hiddenNumber: 7,
+            cols: 2,
+        };
+    }, [mediaSize]);
+    const { ref, inView } = useInView({
+        /* Optional options */
+        threshold: 0,
+        // triggerOnce: true,
+    });
 
-  useEffect(() => {
-    if (!separate) {
-      updateViewInfo(homeScrollIds.techStack, inView);
-    }
-  }, [inView]);
+    useEffect(() => {
+        if (!separate) {
+            updateViewInfo(homeScrollIds.techStack, inView);
+        }
+    }, [inView]);
 
-  return (
-    <div className="overflow-hidden section-element">
-      <section className="container pt-8 pb-44">
-        <SectionTitle
-          scrollLink={homeScrollIds.techStack}
-          title="Tech Stack"
-          description="Technologies I've been working with recently"
-        />
-        <div
-          className="flex justify-end w-full min-h-[340px] overflow-visible"
-          ref={ref}
-        >
-          <TechContent techs={techs} active={inView} sizeInfo={sizeInfo} />
+    return (
+        <div className="overflow-hidden section-element">
+            <section className="container pt-8 pb-44">
+                <SectionTitle
+                    scrollLink={homeScrollIds.techStack}
+                    title="Tech Stack"
+                    description="Technologies I've been working with recently"
+                />
+                <div
+                    className="flex justify-end w-full min-h-[340px] overflow-visible"
+                    ref={ref}
+                >
+                    <TechContent
+                        techs={techs}
+                        active={inView}
+                        sizeInfo={sizeInfo}
+                    />
+                </div>
+                <div className="flex items-start justify-center w-full pt-12 pb-3">
+                    <Button asChild variant={"shine"} size={"lg"}>
+                        <Link href="/tech">See All Techs</Link>
+                    </Button>
+                </div>
+            </section>
         </div>
-        <div className="flex items-start justify-center w-full pt-12 pb-3">
-          <Button asChild variant={"shine"} size={"lg"}>
-            <Link href="/tech">See All Techs</Link>
-          </Button>
-        </div>
-      </section>
-    </div>
-  );
+    );
 };
 export default HomeTechStack;
--- components/tech/ShowLevelAndName.tsx
+++ components/tech/ShowLevelAndName.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useState } from "react";
 import { Progress } from "../ui/progress";
+import { useInView } from "react-intersection-observer";
 
 type ShowLevelAndNameProps = {
     level: number;
@@ -11,17 +12,32 @@ type ShowLevelAndNameProps = {
 
 const ShowLevelAndName = ({ name, level, max }: ShowLevelAndNameProps) => {
     const [progress, setProgress] = useState<number>(0);
+    const [triggerer, setTriggerer] = useState<boolean>(false);
+
+    const { ref, inView } = useInView({
+        /* Optional options */
+        threshold: 0,
+
+        // triggerOnce: true,
+    });
 
     useEffect(() => {
         setProgress(level);
-    }, [level]);
+    }, [level, triggerer]);
+    useEffect(() => {
+        setTriggerer((prevstate) => !prevstate);
+        if (!inView) {
+            setProgress(0);
+        }
+    }, [inView]);
 
     return (
         <>
             <Progress
+                ref={ref}
                 className="bg-primary-foreground border-2 border-primary "
                 value={progress * 10}
-                indicatorClassName="bg-primary transition-translate-x duration-300 ease-[cubic-bezier(0.1, -0.6, 0.2, 0)]"
+                indicatorClassName="bg-primary transition-translate-x duration-300 ease-[cubic-bezier(0.1, -0.6, 0.2, 0)] delay-700"
             />
             <div className="w-full flex justify-between gap-1">
                 <h3 className="text-sm font-bold text-left">{name}</h3>
