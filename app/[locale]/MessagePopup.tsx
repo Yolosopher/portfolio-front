9c3a87d5042b8ac4992c23b6f8c1e6476152f135
Nika <nika@brillevent.com>
2025-03-22 18:20:00 +0400

    fix: Update MessagePopup to return null and include all props in useEffect dependencies

--- app/[locale]/MessagePopup.tsx
+++ app/[locale]/MessagePopup.tsx
@@ -4,21 +4,20 @@ import { toast } from "@/components/ui/use-toast";
 import { useEffect } from "react";
 
 type MessagePopupProps = {
-    title: string;
-    description: string;
-    variant: "default" | "destructive" | "doubt";
+  title: string;
+  description: string;
+  variant: "default" | "destructive" | "doubt";
 };
 
 const MessagePopup = ({ description, title, variant }: MessagePopupProps) => {
-    useEffect(() => {
-        toast({
-            title,
-            description,
-            variant,
-        });
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, []);
-    return <div>MessagePopup</div>;
+  useEffect(() => {
+    toast({
+      title,
+      description,
+      variant,
+    });
+  }, [description, title, variant]);
+  return null;
 };
 
 export default MessagePopup;
