45dd907d451fb6c0b6dd2f9ead8101cbfd5c1c8a
Nika <nika.nishnianidze.10@gmail.com>
2025-08-30 11:59:01 +0400

    feat: Add local font support for Poppins and DM Sans; include font files

--- lib/fonts.ts
+++ lib/fonts.ts
@@ -1,13 +1,55 @@
-import { Poppins } from "next/font/google";
-import { DM_Sans } from "next/font/google";
+import localFont from "next/font/local";
 
-export const poppins = Poppins({
-    weight: ["400", "500", "600", "700"],
-    style: "normal",
-    subsets: ["latin"],
+export const poppins = localFont({
+    src: [
+        {
+            path: "../public/fonts/Poppins-Regular.ttf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/Poppins-Medium.ttf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/Poppins-SemiBold.ttf",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/Poppins-Bold.ttf",
+            weight: "700",
+            style: "normal",
+        },
+    ],
+    display: "swap",
+    preload: true,
 });
 
-export const dmSans = DM_Sans({
-    subsets: ["latin"],
-    weight: ["400", "500", "600", "700"],
+export const dmSans = localFont({
+    src: [
+        {
+            path: "../public/fonts/DMSans-Regular.ttf",
+            weight: "400",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/DMSans-Medium.ttf",
+            weight: "500",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/DMSans-SemiBold.ttf",
+            weight: "600",
+            style: "normal",
+        },
+        {
+            path: "../public/fonts/DMSans-Bold.ttf",
+            weight: "700",
+            style: "normal",
+        },
+    ],
+    display: "swap",
+    preload: true,
 });
